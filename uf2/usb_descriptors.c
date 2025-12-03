a46a6584fd96ca12c4a7129e7fca4aafa4723363
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2025-12-03 16:00:23 +0000

    upd

--- uf2/usb_descriptors.c
+++ uf2/usb_descriptors.c
@@ -6,7 +6,7 @@
 #include "tusb.h"
 
 // --------------------------------------------------------------------
-// Interface numbering (required for TinyUSB audio)
+// Interface numbering
 // --------------------------------------------------------------------
 enum
 {
@@ -16,29 +16,35 @@ enum
 };
 
 // --------------------------------------------------------------------
-// TinyUSB needs the total length of the audio descriptor
-// (TUD_AUDIO_MIC_ONE_CH_DESC_LEN already includes IAD + AC + AS + EP)
+// TinyUSB requires us to explicitly provide the Audio Function Length.
+// For 1-channel microphone descriptor, the macro already exists:
+// TUD_AUDIO_MIC_ONE_CH_DESC_LEN
 // --------------------------------------------------------------------
-#define CONFIG_TOTAL_LEN  (TUD_CONFIG_DESC_LEN + TUD_AUDIO_MIC_ONE_CH_DESC_LEN)
+#define TUD_AUDIO_FUNC_DESC_LEN  TUD_AUDIO_MIC_ONE_CH_DESC_LEN
+#define CONFIG_TOTAL_LEN         (TUD_CONFIG_DESC_LEN + TUD_AUDIO_FUNC_DESC_LEN)
 
 
 //--------------------------------------------------------------------
-// Device Descriptors
+// Device Descriptor
 //--------------------------------------------------------------------
-tusb_desc_device_t const desc_device = {
+tusb_desc_device_t const desc_device =
+{
     .bLength            = sizeof(tusb_desc_device_t),
     .bDescriptorType    = TUSB_DESC_DEVICE,
-    .bcdUSB             = 0x0200,  // USB 2.0
+    .bcdUSB             = 0x0200,
     .bDeviceClass       = TUSB_CLASS_MISC,
     .bDeviceSubClass    = MISC_SUBCLASS_COMMON,
     .bDeviceProtocol    = MISC_PROTOCOL_IAD,
     .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
+
     .idVendor           = 0xCAFE,
     .idProduct          = 0x4001,
     .bcdDevice          = 0x0100,
+
     .iManufacturer      = 0x01,
     .iProduct           = 0x02,
     .iSerialNumber      = 0x03,
+
     .bNumConfigurations = 0x01
 };
 
@@ -47,24 +53,23 @@ uint8_t const * tud_descriptor_device_cb(void)
     return (uint8_t const *) &desc_device;
 }
 
+
 //--------------------------------------------------------------------
 // Configuration Descriptor
 //--------------------------------------------------------------------
 uint8_t const desc_configuration[] =
 {
-    // Config number, interface count, string index, total length, attribute, power in mA
+    // Config number, interfaces, string index, total length, attributes, power
     TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
 
     // Audio Microphone (1 channel)
-    // ITF_NUM_AUDIO_CONTROL = 0
-    // ITF_NUM_AUDIO_STREAMING = 1
     TUD_AUDIO_MIC_ONE_CH_DESCRIPTOR(
-        ITF_NUM_AUDIO_CONTROL,   // Audio Control interface number
-        0,                       // String index
-        2,                       // Bytes per sample
-        16,                      // Bits used per sample
-        0x81,                    // Endpoint IN address
-        512                      // Endpoint size
+        ITF_NUM_AUDIO_CONTROL,    // Audio Control interface
+        0,                        // String index
+        2,                        // Bytes per sample
+        16,                       // Bits used per sample
+        0x81,                     // Endpoint address (IN 1)
+        512                       // Endpoint size
     )
 };
 
@@ -74,11 +79,13 @@ uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
     return desc_configuration;
 }
 
+
 //--------------------------------------------------------------------
 // String Descriptors
 //--------------------------------------------------------------------
-char const* string_desc_arr [] = {
-    (const char[]){ 0x09, 0x04 },  // 0: Language (English)
+char const* string_desc_arr[] =
+{
+    (const char[]){ 0x09, 0x04 },  // 0: English
     "Raspberry Pi",                // 1: Manufacturer
     "Pico W USB Microphone",       // 2: Product
     "123456",                      // 3: Serial
@@ -105,13 +112,12 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
 
         const char* str = string_desc_arr[index];
         chr_count = strlen(str);
-        if (chr_count > 31)
-            chr_count = 31;
+        if (chr_count > 31) chr_count = 31;
 
-        for(uint8_t i = 0; i < chr_count; i++)
-            _desc_str[1+i] = str[i];
+        for (uint8_t i = 0; i < chr_count; i++)
+            _desc_str[1 + i] = str[i];
     }
 
-    _desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
+    _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * chr_count + 2);
     return _desc_str;
 }
